a7f9353009e77cb51c4ad3bbdfce07d3cf808a62
TinyMan <raph.a.uzan@gmail.com>
2018-05-05 12:39:05 +0200

    build(devtools): add chrome extension reload plugin

--- packages/devtools/extension/webpack/webpack.dev.js
+++ packages/devtools/extension/webpack/webpack.dev.js
@@ -1,9 +1,10 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.base.js');
 const webpack = require('webpack');
+const ChromeExtensionReloader = require('webpack-chrome-extension-reloader');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   mode: 'development',
-  plugins: [],
+  plugins: [new ChromeExtensionReloader()],
 });
