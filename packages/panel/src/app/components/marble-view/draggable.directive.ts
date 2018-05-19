6ec206374abbe880b38ad0fc506c744d9cb47165
TinyMan <raph.a.uzan@gmail.com>
2018-05-19 10:57:21 +0200

    refactor(panel): move dragging logic to service

--- packages/panel/src/app/components/marble-view/draggable.directive.ts
+++ packages/panel/src/app/components/marble-view/draggable.directive.ts
@@ -1,56 +1,29 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
+import { MarbleViewService } from './marble-view.service';
 
 @Directive({
   selector: '[draggable]',
 })
 export class DraggableDirective {
-  private dragging = false;
-  private svg: SVGSVGElement;
-  private point: SVGPoint;
-  private viewBox: SVGRect;
-  private pointerOrigin: any;
-  constructor(private el: ElementRef) {
-    this.svg = el.nativeElement;
-    this.point = this.svg.createSVGPoint();
-    this.viewBox = this.svg.viewBox.baseVal;
+  constructor(
+    private el: ElementRef,
+    private marbleViewService: MarbleViewService
+  ) {
+    this.marbleViewService.registerSvg(el.nativeElement);
   }
 
   @HostListener('mousedown', ['$event'])
   mouseDown(event: MouseEvent | TouchEvent) {
-    this.dragging = true;
-    this.pointerOrigin = this.getPointFromEvent(event);
+    this.marbleViewService.startDrag(event);
   }
   @HostListener('mouseup')
   @HostListener('mouseleave')
   mouseUp() {
-    this.dragging = false;
+    this.marbleViewService.stopDrag();
   }
 
   @HostListener('mousemove', ['$event'])
   mousemove(event: MouseEvent | TouchEvent) {
-    if (!this.dragging) {
-      return;
-    }
-    event.preventDefault();
-
-    const pointerPosition = this.getPointFromEvent(event);
-
-    this.viewBox.x -= pointerPosition.x - this.pointerOrigin.x;
-    const temp = this.viewBox.y - (pointerPosition.y - this.pointerOrigin.y);
-    this.viewBox.y = Math.min(0, temp);
-  }
-
-  getPointFromEvent(event: MouseEvent | TouchEvent) {
-    if (event instanceof TouchEvent) {
-      this.point.x = event.targetTouches[0].clientX;
-      this.point.y = event.targetTouches[0].clientY;
-    } else {
-      this.point.x = event.clientX;
-      this.point.y = event.clientY;
-    }
-
-    const invertedSVGMatrix = this.svg.getScreenCTM().inverse();
-
-    return this.point.matrixTransform(invertedSVGMatrix);
+    this.marbleViewService.drag(event);
   }
 }
--- packages/panel/src/app/components/marble-view/marble-view.service.ts
+++ packages/panel/src/app/components/marble-view/marble-view.service.ts
@@ -9,9 +9,64 @@ export class MarbleViewService {
   }
   public notify = new BehaviorSubject(null);
   public startTime = Date.now();
+
+  private point?: SVGPoint;
+  private svg?: SVGSVGElement;
+  private viewBox?: SVGRect;
+  private dragging = false;
+  private pointerOrigin?: SVGPoint;
+
   constructor() {}
   zoom(z: number) {
     this._scale = Math.max(this._scale + z, 1);
     this.notify.next(null);
+  }
+
+  registerSvg(svg: SVGSVGElement) {
+    this.svg = svg;
+    this.point = this.svg.createSVGPoint();
+    this.viewBox = this.svg.viewBox.baseVal;
+  }
+
+  startDrag(event: MouseEvent | TouchEvent) {
+    this.dragging = true;
+    this.pointerOrigin = this.getPointFromEvent(event);
+  }
+  stopDrag() {
+    this.dragging = false;
+  }
+
+  drag(event: MouseEvent | TouchEvent) {
+    if (
+      !this.dragging ||
+      !this.svg ||
+      !this.point ||
+      !this.viewBox ||
+      !this.pointerOrigin
+    ) {
+      return;
+    }
+    event.preventDefault();
+
+    const pointerPosition = this.getPointFromEvent(event)!;
+
+    this.viewBox.x -= pointerPosition.x - this.pointerOrigin.x;
+    const temp = this.viewBox.y - (pointerPosition.y - this.pointerOrigin.y);
+    this.viewBox.y = Math.min(0, temp);
+  }
+
+  getPointFromEvent(event: MouseEvent | TouchEvent): SVGPoint | undefined {
+    if (!this.point || !this.svg) return;
+    if (event instanceof TouchEvent) {
+      this.point.x = event.targetTouches[0].clientX;
+      this.point.y = event.targetTouches[0].clientY;
+    } else {
+      this.point.x = event.clientX;
+      this.point.y = event.clientY;
+    }
+
+    const invertedSVGMatrix = this.svg.getScreenCTM().inverse();
+
+    return this.point.matrixTransform(invertedSVGMatrix);
   }
 }
