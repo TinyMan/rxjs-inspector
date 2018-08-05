9c43ad0c83f512bf42196c56000dfb8e17dc6e57
TinyMan <raph.a.uzan@gmail.com>
2018-08-05 18:28:57 +0200

    fix: demo: remove duplicate component bootstrap

--- demo-app/app/app.module.ts
+++ demo-app/app/app.module.ts
@@ -9,20 +9,10 @@ import { AppComponent } from './app.component';
     BrowserModule,
     RouterModule.forRoot([
       {
-        path: '',
-        component: AppComponent,
-        children: [
-          {
-            path: 'dashboard',
-            loadChildren: './lazy.module#LazyModule',
-          },
-          {
-            path: '**',
-            pathMatch: 'full',
-            redirectTo: 'dashboard',
-          },
-        ],
+        path: 'dashboard',
+        loadChildren: './lazy.module#LazyModule',
       },
+      { path: '**', pathMatch: 'full', redirectTo: 'dashboard' },
     ]),
   ],
   providers: [],
