05b73f6dedf00c5378dd6bcf7a71cd26d847458e
TinyMan <raph.a.uzan@gmail.com>
2018-08-20 18:46:11 +0200

    fix(core): temporary workaround for Microsoft/TypeScript#25769

--- packages/core/src/patch/index.ts
+++ packages/core/src/patch/index.ts
@@ -1,11 +1,13 @@
-import { Observable } from 'rxjs';
+import { Observable, ConnectableObservable } from 'rxjs';
 
 import { lift } from './lift';
 import { patch, Notif, NotificationKind } from './subscribe';
 
 export { Notif, NotificationKind };
 
-export function setup() {
+export function setup(): {
+  notifications$: ConnectableObservable<Notif>;
+} {
   lift(Observable.prototype, {});
   return {
     notifications$: patch(Observable.prototype),
